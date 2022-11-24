d265f9f4783b9de7596e6e3857b6c401adb743c9
Anthony Tran <quanghust96@gmail.com>
2022-11-24 22:00:05 +0700

    code hoan thien

--- ClockJS.js
+++ ClockJS.js
@@ -1,17 +1,29 @@
 window.onload=function(){
-    let hours=document.getElementById("hours");
-    let minutes=document.getElementById("minutes");
-    let seconds=document.getElementById("seconds");
-    let mor=document.getElementById("mor");
+    // let hours=document.getElementById("hours");
+    // let minutes=document.getElementById("minutes");
+    // let seconds=document.getElementById("seconds");
+    // let mor=document.getElementById("mor");
+    let clock=document.getElementById("clock");
     setInterval(setTime,1000);
     function setTime(){
-        let clock=new Date();
-        hours.innerText=display(clock.getHours()%12);
-        minutes.innerText=display(clock.getMinutes());
-        seconds.innerText=display(clock.getSeconds());
-        if ((clock.getHours/12)<1) mor.innerText="AM"
-        else mor.innerText="PM"
-    };
+        let date=new Date();
+        let hours= date.getHours()%12;
+        let minutes=date.getMinutes();
+        let seconds=date.getSeconds();
+        let mor;
+        if (date.getHours/12<1) mor="AM"
+        else mor="PM";
+        clock.innerText=`${display(hours)}:${display(minutes)}:${display(seconds)} ${mor}`;
+    }
+    // function setTime(){
+    //     let clock=new Date();
+    //     hours.innerText=display(clock.getHours()%12);
+    //     minutes.innerText=display(clock.getMinutes());
+    //     seconds.innerText=display(clock.getSeconds());
+    //     if ((clock.getHours/12)<1) mor.innerText="AM"
+    //     else mor.innerText="PM"
+    // };
+
     function display(elem){
         if (elem<10) return "0"+elem;
         else return elem;
